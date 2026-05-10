c4a1c6b73cd63681fa401cc5b764ce28586a50b1
muyufeng <yufeng_135@126.com>
2026-05-10 20:38:46 +0800

    optimization

--- public/js/util.js
+++ public/js/util.js
@@ -1,14 +1,13 @@
 /**
  * Excel 公式计算：IF(flag, CEILING(b4*(1+b2)/g5, 1)*2+b8, CEILING(b4*(1+b2)/g5, 1)+b8)
  * @param {boolean} flag - 为 true 时执行 CEILING*2+B8，为 false 时执行 CEILING+B8
- * @param {number} pointBuff - 对应 Excel B4 单元格
+ * @param {number} points - 对应 Excel B4 单元格
  * @param {number} spare - 对应 Excel $B$2 单元格，此值是一个百分数
- * @param {number} inputModule - 对应 Excel G5 单元格
+ * @param {number} slotCount - 对应 Excel G5 单元格
  * @param {number} moduleAdd - 对应 Excel B8 单元格
  * @returns {number}
  */
 function calcMouduleQuantity(flag, points, spare, slotCount, moduleAdd) {
-    debugger
     points = points || 0;
     spare = spare || 0;
     moduleAdd = moduleAdd || 0;
